1dd5c2665ee393a5f2d2a54a88a42367d923eba2
piotr04j <darricck04@gmail.com>
2022-06-18 19:34:22 +0200

    added draft of db setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        database: 'board_db',
+        host: 'db',
+        port: 5432,
+        username: 'postgres',
+        password: 'qwe123',
+        entities: [User],
+        synchronize: true
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
